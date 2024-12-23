7079936d4cef59237fd6d4d0b2673be4e339ba32
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2024-12-24 04:00:05 +0530

    Update auth.js

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,1 +1,10 @@
+const express = require("express");
 
+const router = express.Router();
+const authControllers = require("../Controllers/auth");
+
+router.post("/register",authControllers.register);
+
+router.post("/login",authControllers.login);
+
+module.exports = router;
