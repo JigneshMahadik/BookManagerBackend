7b864dd70c901594f2dec6b0078a9409528eb946
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2024-12-24 03:54:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+// const authRoutes = require("./Routes/auth");
+const { default: mongoose } = require("mongoose");
+const cors = require("cors");
+const app = express();
+const booksRoutes = require("./Routes/books");
+
+app.use(express.json());
+
+// Use the CORS middleware
+app.use(cors({
+    origin: 'https://book-manager-rouge.vercel.app/', // Replace with the origin you want to allow
+    // origin : "http://localhost:3000",
+    // allowedHeaders: 'Content-Type,Authorization'
+    // optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    methods : ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
+}));
+
+// app.use(authRoutes);
+app.use(booksRoutes);
+
+
+mongoose.connect("mongodb://localhost:27017/bookManager")
+.then(()=>{ console.log("DB Connection Successful"); })
+.catch((error)=>{ console.log("Error while DB Connection !", error); })
+
+app.listen(10000,()=>{
+    console.log("Server is up and running on port : 10000");
+});
