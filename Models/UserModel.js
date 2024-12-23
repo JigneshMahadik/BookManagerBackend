bbfc966c046de3c2d7740435e7d034619ca773dc
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2024-12-24 03:56:58 +0530

    Add files via upload

---
+++ Models/UserModel.js
@@ -0,0 +1,7 @@
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  });
+  
+  module.exports = mongoose.model('User', userSchema);
+  
